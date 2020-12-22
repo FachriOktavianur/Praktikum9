521b23ced4d2fcbcbab33ff1afcc2f9b99e2a267
FachriOktavianur <71803235+FachriOktavianur@users.noreply.github.com>
2020-12-22 23:56:53 +0700

    Create no3.py

---
+++ no3.py
@@ -0,0 +1,13 @@
+def bintang(n):
+    n= n//2 +1
+    for bintang in range (n):
+       bintang = bintang + 1
+       print(('*' * (2*bintang-1)).center(2*n-1, ' '))
+
+     
+    for bintang2 in range (n,0,-1):
+        bintang2 = bintang2-1
+        print((''*(n-bintang2) + '*'*(bintang2)+'*'*(bintang2-1)).center(2*n-1, ' '))     
+bintang(7)
+
+
